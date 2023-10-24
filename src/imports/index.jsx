d186e5ecd66b4430b461e411e8301ff98a803e3a
NguyenVoTien <tienvo28052000@gmail.com>
2023-10-25 01:16:48 +0700

    #4 Upload Component and Layout

---
+++ src/imports/index.jsx
@@ -0,0 +1,30 @@
+//* Import Page
+export { default as HomePage } from '@/pages/Home';
+export { default as NotfoundPage } from '@/pages/NotFound';
+export { default as LoginPage } from '@/pages/Login';
+export { default as AboutPage } from '@/pages/About';
+export { default as DetailPage } from '@/pages/Details';
+// import DetailP from '@/pages/Details';
+// Imports useEffect
+export { default as ExerciseOneUseEffect } from '@/pages/useEffect/exerciseOne';
+export { default as ExerciseTwoUSeEffect } from '@/pages/useEffect/exerciseTwo';
+export { default as ExerciseThreeUseEffect } from '@/pages/useEffect/exerciseThree';
+// Imports useState
+export { default as ExerciseOneUseState } from '@/pages/useState/exerciseOne';
+export { default as ExerciseTwoUseState } from '@/pages/useState/exerciseTwo';
+export { default as ExerciseThreeUseState } from '@/pages/useState/exerciseThree';
+// Import useRef
+export { default as ExerciseOneUseRef } from '@/pages/useRef/exerciseOne';
+export { default as ExerciseTwoUseRef } from '@/pages/useRef/exerciseTwo';
+export { default as ExerciseThreeUseRef } from '@/pages/useRef/exerciseThree';
+export { default as ExerciseFourUseRef } from '@/pages/useRef/exerciseFour';
+export { default as ExerciseFiveUseRef } from '@/pages/useRef/exerciseFive';
+export { default as ExerciseSixUseRef } from '@/pages/useRef/exerciseSix';
+// Imports useReduce
+export { default as ExerciseOneUseReducer } from '@/pages/useReducer/exerciseOne';
+
+//Import Navbar
+export { default as NavBar } from '@/layouts/navbars';
+
+// export { default as RouterPages } from '@/routers/index';
+// export { default as IndexRouter } from '@router/IndexRouter.jsx';
