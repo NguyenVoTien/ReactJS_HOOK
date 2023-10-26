1e7b90ad605d1163bc88a0de9b152a2ca60a3a13
NguyenVoTien <tienvo28052000@gmail.com>
2023-10-26 21:53:02 +0700

    #4 Upload componet and Import

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,35 @@
 //* Lib
-import { Suspense } from 'react';
+// import { Suspense } from 'react';
 //* CSS
 import './App.css';
+//* HOOK
+import { useDispatch } from 'react-redux';
 
 //* Router
-import RouterPages from '@/routers/index';
-import Loading from '@/components/loadings';
+// import RouterPages from '@/routers/index';
+// import Loading from '@/components/loadings';
+//* Store
+import { addTodoPending, addTodoSuccess, addTodoError } from './stores/ActionType';
 
 function App() {
+  const dispatch = useDispatch();
+  const handleAddTodo = () => {
+    dispatch(addTodoPending());
+    try {
+      setTimeout(() => {
+        dispatch(addTodoSuccess());
+      }, 2000);
+    } catch (error) {
+      dispatch(addTodoError(error));
+    }
+  };
   return (
     <>
+      {/* RouterPages
       <Suspense fallback={<Loading />}>
         <RouterPages />
-      </Suspense>
+      </Suspense> */}
+      <button onClick={handleAddTodo}>ADD TODO</button>
     </>
   );
 }
--- src/components/loadings/index.jsx
+++ src/components/loadings/index.jsx
@@ -1,5 +1,19 @@
+import './styles.css';
 const Loading = () => {
-  return <div>Loading</div>;
+  return (
+    <div className="waviy">
+      <span style={{ '--i': 1 }}>L</span>
+      <span style={{ '--i': 2 }}>O</span>
+      <span style={{ '--i': 3 }}>A</span>
+      <span style={{ '--i': 4 }}>D</span>
+      <span style={{ '--i': 5 }}>I</span>
+      <span style={{ '--i': 6 }}>N</span>
+      <span style={{ '--i': 7 }}>D</span>
+      <span style={{ '--i': 8 }}>.</span>
+      <span style={{ '--i': 9 }}>.</span>
+      <span style={{ '--i': 10 }}>.</span>
+    </div>
+  );
 };
 
 export default Loading;
